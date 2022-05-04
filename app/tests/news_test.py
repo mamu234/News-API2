5dd46c17f2b16b52882943e00cc1dcf13ae042ff
carolyne <pcmaunda@gmail.com>
2022-05-04 08:46:06 +0300

    modified navbar

--- app/tests/news_test.py
+++ app/tests/news_test.py
@@ -1,3 +1,18 @@
 import unittest
 from ..models import Articles
 
+
+class NewsTest(unittest.TestCase):
+    def setUp(self):
+        self.articles = Articles(
+          "cnn.com", 
+            "Stephen Collinson", 
+            "Supreme Court's draft opinion sends electric shock through midterm campaigns" 
+            "https://us.cnn.com/2022/05/04/politics/supreme-court-draft-opinion-midterm-shock/index.html"
+            
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.articles, Articles))
+
+if __name__ == "__main__":
+    unittest.main()
