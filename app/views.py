79d3f07fb1074f0325e3a40653bfa75ce7c5d30b
carolyne <pcmaunda@gmail.com>
2022-05-04 10:58:07 +0300

    created route for categories

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template ,request,redirect,url_for
+from flask import render_template 
 from .request import businessArticles, entArticles, get_news_source, healthArticles, publishedArticles, randomArticles, scienceArticles, sportArticles, techArticles, topHeadlines
 
 
@@ -12,17 +12,19 @@ def home():
 
     return  render_template('home.html', articles = articles)
 
-@app.route('/headlines')
-def headlines():
-    headlines = topHeadlines()
 
-    return  render_template('headlines.html', headlines = headlines)
 
 @app.route('/articles')
 def articles():
     random = randomArticles()
 
     return  render_template('articles.html', random = random)
+
+@app.route('/headlines')
+def headlines():
+    headlines = topHeadlines()
+
+    return  render_template('headlines.html', headlines = headlines)
 
 @app.route('/sources')
 def sources():
