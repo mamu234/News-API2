d9cfbcfba8e61f5e6ba1c088e943c6ba7f045fe7
carolyne <pcmaunda@gmail.com>
2022-05-05 20:30:11 +0300

    deployed app

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config:
     NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     API_KEY = "6cc037b2e9984d0b995fb8d87f06d897"
+    SECRET_KEY = '123'
 
 class ProdConfig(Config):
     pass
